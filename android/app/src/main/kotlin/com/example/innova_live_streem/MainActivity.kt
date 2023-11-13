c68c04766a51bd03f80e1fd616dee1f839d7a52a
Mosh. Fatema Akhter Koli <105162409+Mosh-Fatema-Koli@users.noreply.github.com>
2023-11-13 14:37:32 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/innova_live_streem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.innova_live_streem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:innova_live_streem/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
